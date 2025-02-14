b943a069f61355f2ef4973ae066e940966c6a338
Rodrigo Mungo <rodrigomungo@gmail.com>
2025-02-14 02:32:39 -0300

    feat: :sparkles: change activity indicator color with backdrop

--- app/auth/sign-in/index.tsx
+++ app/auth/sign-in/index.tsx
@@ -195,7 +195,7 @@ const SignInScreen = () => {
               left: 0,
               right: 0,
               bottom: 0,
-              backgroundColor: 'rgba(0, 0, 0, 0.4)',
+              backgroundColor: theme.colors.backdrop,
               justifyContent: 'center',
               alignItems: 'center',
               zIndex: 1000,
